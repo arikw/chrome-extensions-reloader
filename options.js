2bd28d132a4f5572eee132ea68b74fbea65fd62e
arikw <arik_v@yahoo.com>
2021-07-25 23:23:46 +0300

    beautify options page code

--- options.js
+++ options.js
@@ -1,29 +1,29 @@
 // Saves options to chrome.storage.sync.
-function save_options() {
+function saveOptions() {
   chrome.extension.getBackgroundPage().console.log('in save options...');
 
-  var reloadPageVal = document.getElementById('reload_page_after_extension_reload').checked;
+  const reloadPageVal = document.getElementById('reload_page_after_extension_reload').checked;
   chrome.storage.sync.set({
     'reloadPage': reloadPageVal
-  }, function() {
+  }, function () {
     // Update status to let user know options were saved.
-    var statusEl = document.getElementById('status');
+    const statusEl = document.getElementById('status');
 
     statusEl.textContent = 'Options saved.';
-    setTimeout(function() {
+    setTimeout(function () {
       statusEl.textContent = '';
     }, 1000);
   });
 }
 
 // Restores select box and checkbox state using the preferences stored in chrome.storage.
-function restore_options() {
+function restoreOptions() {
   chrome.storage.sync.get({
     'reloadPage': false
-  }, function(items) {
+  }, function (items) {
     document.getElementById('reload_page_after_extension_reload').checked = items.reloadPage;
   });
 }
 
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click', save_options);
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.getElementById('save').addEventListener('click', saveOptions);
